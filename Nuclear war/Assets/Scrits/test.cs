c171fe8844650544cb435c1a4a3d4cc6b404de65
Tezr0 <112214955+Tezr0@users.noreply.github.com>
2023-02-18 18:11:15 +0300

    Кнопка Бидон рокет
    Импортировал TMP
    Поменял переменную Monyeна money
    Создал кнопку для рокет

--- Nuclear war/Assets/Scrits/test.cs
+++ Nuclear war/Assets/Scrits/test.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    [SerializeField] public int Monye, Pro, Rocket, Factory, GrowthPro = 1, GrowthRocket = 1, GrowthFactory = 1;
+    [SerializeField] public int Money = 0, Pro, Rocket, Factory, GrowthPro = 1, GrowthRocket = 1, GrowthFactory = 1;
     public const int RATIO_ATTACK_ROKET_PRO = 2, RATIO_ATTACK_ROKET_FACTORY = 10;
 }
