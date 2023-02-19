b8bc7a96193c53146fcb9516b924c6297515629c
GywaCahiMo <nikita-l-1987@mail.ru>
2023-02-19 11:16:30 +0300

    v 1.0.3
    добавненны текстуры и tmp

--- Nuclear war/Assets/Scrits/RocketMenu.cs
+++ Nuclear war/Assets/Scrits/RocketMenu.cs
@@ -1,15 +1,14 @@
 using System.Collections;
-using System.Net.Sockets;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class RocketMenu : MonoBehaviour
 {
     public test MainScript;
     
     [Header("Текста")]
-    [SerializeField] private Text CounterRocket;
-    [SerializeField] private Text CounterRocketGrowth;
+    [SerializeField] private TMP_Text CounterRocket;
+    [SerializeField] private TMP_Text CounterRocketGrowth;
 
     private void Start()
     {
